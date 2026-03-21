7ee3d89d49632d9623f6b82caf70fe32ab15dbbe
noraidouaouzal-etu-lab <nora.idouaouzal-etu@etu.univh2c.ma>
2026-03-21 22:01:21 +0100

    Create variables.tf

---
+++ terraform/variables.tf
@@ -0,0 +1,9 @@
+variable "location" {
+  description = "Region Azure"
+  default     = "norwayeast"
+}
+
+variable "vm_size" {
+  description = "Taille des VMs"
+  default     = "Standard_B2ats_v2"
+}
