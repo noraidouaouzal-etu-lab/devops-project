72c9bd7ab35036766102e21755573e825e6d6c79
Hiba2500 <hiba.bounaga-etu@etu.univh2c.ma>
2026-03-22 00:20:30 +0000

    Update variable descriptions to French

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,9 +1,20 @@
+# DÉFINITION DES VARIABLES
+
+
 variable "location" {
-  description = "Region Azure"
+  description = "La région Azure pour le déploiement"
+  type        = string
   default     = "norwayeast"
 }
 
+variable "resource_group_name" {
+  description = "Le nom du groupe de ressources"
+  type        = string
+  default     = "DevOps-Project-RG"
+}
+
 variable "vm_size" {
-  description = "Taille des VMs"
+  description = "La taille de la machine virtuelle"
+  type        = string
   default     = "Standard_B2ats_v2"
 }
