517057831f1234ba5163c8c1da514c433b5b6470
Hiba2500 <hiba.bounaga-etu@etu.univh2c.ma>
2026-03-22 00:19:41 +0000

    Refactor Azure resources to use variables and update rules
    Updated Azure resource configurations to use variables for resource group name and VM size. Modified security group rules and added public IPs for master and worker nodes.

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,4 @@
+# 1. Configuration du fournisseur Azure
 terraform {
   required_providers {
     azurerm = {
@@ -8,14 +9,20 @@ terraform {
 }
 
 provider "azurerm" {
-  features {}
+  features {
+    resource_group {
+      prevent_deletion_if_contains_resources = false 
+    }
+  }
 }
 
+# 2. Création du Groupe de Ressources (Utilise la variable)
 resource "azurerm_resource_group" "rg" {
-  name     = "DevOps-Project-RG"
+  name     = var.resource_group_name
   location = var.location
 }
 
+# 3. Configuration du Réseau Virtuel (VNET)
 resource "azurerm_virtual_network" "vnet" {
   name                = "k8s-vnet"
   address_space       = ["10.0.0.0/16"]
@@ -30,6 +37,7 @@ resource "azurerm_subnet" "subnet" {
   address_prefixes     = ["10.0.1.0/24"]
 }
 
+# 4. Groupe de Sécurité Réseau (NSG)
 resource "azurerm_network_security_group" "nsg" {
   name                = "k8s-nsg"
   location            = azurerm_resource_group.rg.location
@@ -37,7 +45,7 @@ resource "azurerm_network_security_group" "nsg" {
 
   security_rule {
     name                       = "Allow-SSH"
-    priority                   = 100
+    priority                   = 1001
     direction                  = "Inbound"
     access                     = "Allow"
     protocol                   = "Tcp"
@@ -48,8 +56,8 @@ resource "azurerm_network_security_group" "nsg" {
   }
 
   security_rule {
-    name                       = "Allow-K8s"
-    priority                   = 110
+    name                       = "Allow-K8s-API"
+    priority                   = 1002
     direction                  = "Inbound"
     access                     = "Allow"
     protocol                   = "Tcp"
@@ -59,16 +67,42 @@ resource "azurerm_network_security_group" "nsg" {
     destination_address_prefix = "*"
   }
 
+
+
   security_rule {
-    name                       = "Allow-NodePort"
-    priority                   = 120
+    name                       = "Allow-K8s-Service"
+    priority                   = 1003
     direction                  = "Inbound"
     access                     = "Allow"
     protocol                   = "Tcp"
     source_port_range          = "*"
-    destination_port_range     = "30000-32767"
+    destination_port_range     = "31951"
     source_address_prefix      = "*"
     destination_address_prefix = "*"
+  }
+ } 
+# ==========================================================
+# CONFIGURATION DES NODES (MASTER & WORKER)
+# ==========================================================
+
+# -- MASTER --
+resource "azurerm_public_ip" "pip_master" {
+  name                = "master-pip"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  allocation_method   = "Static"
+  sku                 = "Standard"
+}
+
+resource "azurerm_network_interface" "nic_master" {
+  name                = "master-nic"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.subnet.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.pip_master.id
   }
 }
 
@@ -76,9 +110,10 @@ resource "azurerm_linux_virtual_machine" "master" {
   name                = "k8s-master"
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
-  size                = var.vm_size
+  size                = var.vm_size # Utilise la variable
+  admin_username      = "azureuser"
+  network_interface_ids = [azurerm_network_interface.nic_master.id]
 
-  admin_username = "azureuser"
   admin_ssh_key {
     username   = "azureuser"
     public_key = file("~/.ssh/id_rsa.pub")
@@ -95,9 +130,26 @@ resource "azurerm_linux_virtual_machine" "master" {
     sku       = "22_04-lts"
     version   = "latest"
   }
+}
 
-  tags = {
-    Role = "master"
+# -- WORKER --
+resource "azurerm_public_ip" "pip_worker" {
+  name                = "worker-pip"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  allocation_method   = "Static"
+  sku                 = "Standard"
+}
+
+resource "azurerm_network_interface" "nic_worker" {
+  name                = "worker-nic"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.subnet.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.pip_worker.id
   }
 }
 
@@ -105,9 +157,10 @@ resource "azurerm_linux_virtual_machine" "worker" {
   name                = "k8s-worker"
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
-  size                = var.vm_size
+  size                = var.vm_size # Utilise la variable
+  admin_username      = "azureuser"
+  network_interface_ids = [azurerm_network_interface.nic_worker.id]
 
-  admin_username = "azureuser"
   admin_ssh_key {
     username   = "azureuser"
     public_key = file("~/.ssh/id_rsa.pub")
@@ -123,9 +176,5 @@ resource "azurerm_linux_virtual_machine" "worker" {
     offer     = "0001-com-ubuntu-server-jammy"
     sku       = "22_04-lts"
     version   = "latest"
-  }
-
-  tags = {
-    Role = "worker"
   }
 }
