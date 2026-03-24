c25cbec8c8a7f2796fc4e6aca9dda5eabee76fdd
Nora Idouaouzal <nora.idouaouzal-etu@etu.univh2c.ma>
2026-03-24 18:11:58 +0100

    N: hi

--- app/app.py
+++ app/app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return jsonify({
-        "message": "Hello jdas !",
+        "message": "Hello chabiba !",
         "version": os.getenv("APP_VERSION", "1.0.0"),
         "status": "running"
     })
