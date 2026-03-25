92343c6878de462cbb2e4e13ac80eeb6b71aa928
noraidouaouzal-etu-lab <nora.idouaouzal-etu@etu.univh2c.ma>
2026-03-25 12:48:25 +0100

    Update app.py

--- app/app.py
+++ app/app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return jsonify({
-        "message": "Hello devops !",
+        "message": "Hello world !",
         "version": os.getenv("APP_VERSION", "1.0.0"),
         "status": "running"
     })
